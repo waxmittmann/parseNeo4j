fcd46b1e374b8c820daff2f59cbb851fb88b7367
Max Wittmann <damxam@gmail.com>
2018-08-06 08:11:46 +1000

    Works, test passes

--- src/test/scala/mwittmann/neo4japp/parsewitherror/ParseN4jSpec.scala
+++ src/test/scala/mwittmann/neo4japp/parsewitherror/ParseN4jSpec.scala
@@ -5,13 +5,15 @@ import java.util.UUID
 import scala.util.Random
 import scala.collection.JavaConverters._
 
+import cats.syntax._
+import cats.implicits._
+
 import mwittmann.neo4japp.db.WrappedDriver
-import mwittmann.neo4japp.parsewitherror.ParseN4j.{NodeGetDirect, NodeParser, RecordParser, Result}
+import mwittmann.neo4japp.parsewitherror.ParseN4j._
 import mwittmann.neo4japp.parsewitherror.WrappedAtomImpl.Implicits._
 import org.neo4j.driver.v1.Record
 import org.neo4j.driver.v1.summary.ResultSummary
 import org.specs2.mutable.Specification
-
 
 class ParseN4jSpec extends Specification {
 
@@ -24,10 +26,16 @@ class ParseN4jSpec extends Specification {
       val wd: WrappedDriver = WrappedDriver.local
       val uid1 = UUID.randomUUID()
       val uid2 = UUID.randomUUID()
-      wd.unsafeTx { _.run(s"CREATE (v :A :$rl { uid: '${uid1.toString}', attr: 'a' })").summary() }
-      wd.unsafeTx { _.run(s"CREATE (v :A :$rl { uid: '${uid2.toString}', attr: 'b' })").summary() }
+      wd.unsafeTx {
+        _.run(s"CREATE (v :A :$rl { uid: '${uid1.toString}', attr: 'a' })").summary()
+      }
+      wd.unsafeTx {
+        _.run(s"CREATE (v :A :$rl { uid: '${uid2.toString}', attr: 'b' })").summary()
+      }
 
-      val parse = wd.unsafeTx { _.run(s"MATCH (v :$rl :A) RETURN v") }
+      val parse = wd.unsafeTx {
+        _.run(s"MATCH (v :$rl :A) RETURN v")
+      }
       val resultLi: List[Record] = parse.list().asScala.toList
 
 
@@ -56,9 +64,13 @@ class ParseN4jSpec extends Specification {
 
       val wd: WrappedDriver = WrappedDriver.local
       val uid1 = UUID.randomUUID()
-      wd.unsafeTx { _.run(s"CREATE (v :A :$rl { uid: '${uid1.toString}', attr: 'a' })").summary() }
+      wd.unsafeTx {
+        _.run(s"CREATE (v :A :$rl { uid: '${uid1.toString}', attr: 'a' })").summary()
+      }
 
-      val parse = wd.unsafeTx { _.run(s"MATCH (v :$rl :A) RETURN v") }
+      val parse = wd.unsafeTx {
+        _.run(s"MATCH (v :$rl :A) RETURN v")
+      }
       val resultLi: List[Record] = parse.list().asScala.toList
 
 
@@ -82,4 +94,57 @@ class ParseN4jSpec extends Specification {
 
   }
 
+  "when doing a more complex example" should {
+    "work" in {
+      val rl = randomLabel
+
+      val wd: WrappedDriver = WrappedDriver.local
+      val uidA = UUID.randomUUID()
+      val uidB1 = UUID.randomUUID()
+      val uidB2 = UUID.randomUUID()
+      val uidB3 = UUID.randomUUID()
+
+      wd.unsafeStatement(
+        s"""
+           |CREATE
+           |  (a :A :$rl { uid: '${uidA.toString}', attr: 'a' }),
+           |  (a) -[:AB]-> (b1 :B :$rl { uid: '${uidB1.toString}', attr: 'b1' }),
+           |  (a) -[:AB]-> (b2 :B :$rl { uid: '${uidB2.toString}', attr: 'b2' }),
+           |  (a) -[:AB]-> (b3 :B :$rl { uid: '${uidB3.toString}', attr: 'b3' })
+           |""".stripMargin).summary()
+
+      val parse = wd.unsafeTx {
+        _.run(s"MATCH (a :$rl :A) --> (b :$rl :B) RETURN a, collect(b) AS bs")
+      }
+
+      val resultLi: List[Record] = parse.list().asScala.toList
+
+      case class A(uid: UUID, attr: String, bs: List[B])
+      case class B(uid: UUID, attr: String)
+
+      val bParser: NodeParser[B] = { node =>
+        for {
+          uid <- node.get[UUID]("uid")
+          attr <- node.get[String]("attr")
+        } yield B(uid, attr)
+      }
+
+      val aParser: RecordParser[A] = { (record: WrappedRecord) =>
+        for {
+          node <- record.getNode("a")
+          uid <- node.get[UUID]("uid")
+          attr <- node.get[String]("attr")
+          rawBs <- record.getNodes("bs")
+          bs <- rawBs.map(bParser).sequence
+        } yield A(uid, attr, bs)
+      }
+
+      val r: Result[List[A]] = resultLi.map((v: Record) => aParser(new WrappedRecordImpl(v))).sequence
+
+      r must beRight
+      val r2 = r.right.get
+      r2.size mustEqual 1
+      r2.head mustEqual A(uidA, "a", List(B(uidB1, "b1"), B(uidB2, "b2"), B(uidB3, "b3")))
+    }
+  }
 }
